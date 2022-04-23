022f4d2b82665885b1001b568c40820d34eac0cd
Hamish MacDonald <hamish.macdonald@servian.com>
2022-04-23 23:23:42 +1000

    env variables to forward request to storage microservice

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 const express = require("express");
-const fs = require("fs");
+const http = require("http");
+
 const app = express();
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
+
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+}
 const port = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
 app.get("/video", (req, res) => {
-
-    //
-    // Original video from here:
-    // https://sample-videos.com
-    //
-    const path = "./videos/Surfer1.mp4";
-    fs.stat(path, (err, stats) => {
-        if (err) {
-            console.error("An error occurred ");
-            res.sendStatus(500);
-            return;
+    const forwardRequest = http.request( // Forward the request to the video storage microservice.
+        {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: '/video?path=Surfer1.mp4', // Video path is hard-coded for the moment.
+            method: 'GET',
+            headers: req.headers
+        },
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
         }
+    );
 
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
-        });
-        fs.createReadStream(path).pipe(res);
-    });
+    req.pipe(forwardRequest);
 });
 
 app.listen(port, () => {
-    console.log(`Microservice listening on port ${port}, point your browser at http://localhost:${port}/video`);
+    console.log(`HTTP server microservice online.`);
 });
